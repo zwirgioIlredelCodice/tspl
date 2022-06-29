332ae6d63abfefb1128eb7044563704dbe1ecd8d
Fabio Murer <murer.fabio23@gmail.com>
2022-06-29 22:51:13 +0200

    sas

--- src/bytecode.rs
+++ src/bytecode.rs
@@ -1,8 +1,8 @@
 use nom::{
     branch::alt,
     bytes::complete::{escaped_transform, tag, is_not, is_a},
-    character::{complete::char, streaming::alpha1},
-    combinator::{eof, recognize, value},
+    character::{complete::char},
+    combinator::{eof, recognize, value, map},
     multi::{fold_many0, many1},
     sequence::{delimited, separated_pair, terminated},
     IResult,
@@ -10,54 +10,39 @@ use nom::{
 
 use std::collections::HashMap;
 
-fn word_parser(i: &str) -> IResult<&str, &str> {
-    recognize(many1(is_not("\" \t\n\r")))(i)
-}
-
-fn string_parser(i: &str) -> IResult<&str, &str> {
-    delimited(
-        char('"'),
-        recognize(escaped_transform(
-            recognize(many1(is_not("\"\\"))),
-            '\\',
-            alt((
-                value('\\', tag("\\")),
-                value('\"', tag("\"")),
-                value('\n', tag("n")),
-                value('\t', tag("t")),
-                value('\r', tag("r")),
-            )),
-        )),
-        char('"'),
+fn word_parser(i: &str) -> IResult<&str, String> {
+    map(
+        recognize(many1(is_not("\" \t\n\r"))),
+        |s: &str| s.to_string()
     )(i)
 }
 
-fn string1_parser(i: &str) -> IResult<&str, String> {
+fn string_parser(i: &str) -> IResult<&str, String> {
     delimited(
         char('"'),
         escaped_transform(
-            alpha1,
+            recognize(many1(is_not("\"\\"))),
             '\\',
             alt((
-                value('\\', tag("\\")),
-                value('\"', tag("\"")),
-                value('\n', tag("n")),
-                value('\t', tag("t")),
-                value('\r', tag("r")),
+                value("\\", tag("\\")),
+                value("\"", tag("\"")),
+                value("\n", tag("n")),
+                value("\t", tag("t")),
+                value("\r", tag("r")),
             )),
         ),
         char('"'),
     )(i)
 }
 
-fn line_parser(i: &str) -> IResult<&str, (&str, &str)> {
+fn line_parser(i: &str) -> IResult<&str, (String, String)> {
     terminated(
         separated_pair(
             alt((word_parser, string_parser)),
             char(' '),
             alt((word_parser, string_parser)),
         ),
-        is_a("\n\r"),
+        is_a("\n\r\0"),
     )(i)
 }
 
@@ -67,19 +52,9 @@ fn line_parser(i: &str) -> IResult<&str, (&str, &str)> {
 pub fn program_parser(i: &str) -> IResult<&str, HashMap<String, String>> {
     terminated(
         fold_many0(line_parser, HashMap::new, |mut table: HashMap<String, String>, item| {
-            table.insert(String::from(item.0), String::from(item.1));
+            table.insert(item.0, item.1);
             table
         }),
         eof,
     )(i)
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    #[test]
-    fn test_string() {
-        // https://docs.rs/nom/latest/nom/bytes/complete/fn.escaped_transform.html
-        assert_eq!(string1_parser("ab\\\"cd"), Ok(("", String::from("ab\"cd"))));
-    }
 }
